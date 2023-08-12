555383e946223d0be990b07c58f3ec08897d6917
KimHout Sam <samkimhout@gmail.com>
2023-08-12 16:45:21 +0700

    chore: update npm

--- src/middlewares/VerifyMiddleware.ts
+++ src/middlewares/VerifyMiddleware.ts
@@ -1,11 +1,5 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
-declare module 'adonis-rapid' {
-  interface Middlewares {
-    VerifyMiddleware: MiddlewareConstruct<VerifyMiddleware>
-  }
-}
-
 export default class VerifyMiddleware {
   public handle({}: HttpContextContract, next: () => void) {
     return next()
