6a15fececde1f66f9101ed808f34a67a18194586
KimHout Sam <samkimhout@gmail.com>
2023-08-14 15:32:46 +0700

    chore(release): update preview v0.1.5-preview-4 just republish

--- src/controllers/LoginController.ts
+++ src/controllers/LoginController.ts
@@ -22,43 +22,14 @@ export default class LoginController {
     return context.view.render('pages/auth/login', data)
   }
 
-  public async store({ request, response, auth, session }: HttpContextContract) {
-    const Hash = this.app.container.use('Adonis/Core/Hash')
-    const uid: string = request.input('uid')
-    const password: string = request.input('password')
-    const remember: boolean = request.input('remember', false)
-    const { default: UserAppModel } = require(this.app.makePath('app', 'Models', 'User.ts')) as {
-      default: UserModel
-    }
+  public async store({ request, response, auth }: HttpContextContract) {
+    const email = request.input('email')
+    const password = request.input('password')
 
     try {
-      //
-
-      // Attempt And Redirect Home
-      auth.use('web').attempt(uid, password, remember)
-      return response.redirect('/')
-
-      //
+      await auth.use('web').attempt(email, password)
+      return response.redirect().toRoute('dashboard')
     } catch (error) {
-      //
-
-      // get Model By Email
-      const model = await UserAppModel.findBy('email', uid)
-
-      // Check If Model Exist
-      if (model === null) {
-        session.put('errors.uid', 'this uid is invaild since there no user with that uid')
-        return response.redirect().back()
-      }
-
-      // Check If Password Correct With The Model
-      if (await Hash.verify(model.password, password)) {
-        session.put('errors.password', 'this password is invaild to this uid')
-        return response.redirect().back()
-      }
-
-      // Check Other Error Than Client-Side Input Error
-      console.log(error)
       return response.redirect().back()
     }
   }
